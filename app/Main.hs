7e058a633920a54fa8adc2d904f2233f48b307e1
StefanosTouf <stefetoufe@gmail.com>
2022-09-10 19:24:42 +0300

    Get configuration from environment

--- app/Main.hs
+++ app/Main.hs
@@ -5,19 +5,44 @@ module Main
 import           Control.Monad.Reader           ( MonadIO(liftIO)
                                                 , ReaderT(runReaderT)
                                                 )
+import           Data.String
+import qualified Data.Text                     as T
+import           Data.Time
 import           Lib                            ( executeCommand )
 import           Model.Config
 import           Model.DateTime
 import           Parse.ParseCommand             ( parseCommand )
+import           System.Environment             ( getEnv )
+
+getTime :: IO Time
+getTime = fmap (toTime . localTimeOfDay . zonedTimeToLocalTime) getZonedTime
+ where
+  toTime TimeOfDay { todHour, todMin } =
+    Time { hour = toInteger todHour, minute = toInteger todMin }
+
+getDate :: IO Date
+getDate = do
+  now      <- getCurrentTime
+  timezone <- getCurrentTimeZone
+  let zoneNow            = utcToLocalTime timezone now
+  let (year, month, day) = toGregorian $ localDay zoneNow
+  pure $ Date year (toInteger month) (toInteger day)
+
 
 main :: IO ()
-main = runReaderT exec cnf
+main = cnf >>= runReaderT exec
  where
   exec = parseCommand >>= executeCommand >>= (liftIO . print)
-  cnf  = AppConfig { host     = "localhost"
-                   , port     = 8080
-                   , username = "stef1"
-                   , password = "password1"
-                   , date     = Date { year = 2022, month = 9, day = 8 }
-                   , time     = Time { hour = 17, minute = 21 }
-                   }
+  cnf =
+    AppConfig
+      <$> fmap T.pack     (getEnv "HOST")
+      <*> fmap read       (getEnv "PORT")
+      <*> fmap fromString (getEnv "USERNAME")
+      <*> fmap fromString (getEnv "PASSWORD")
+      <*> getDate
+      <*> getTime
+
+
+
+
+
